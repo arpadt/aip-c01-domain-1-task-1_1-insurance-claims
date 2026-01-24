65d503869b07260d4211ebcec16abd691e7f5fdb
Arpad Toth <a.arpad.toth@gmail.com>
2026-01-24 11:59:49 +0100

    Initial commit

---
+++ test/task-1_1-insurance-claims.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Task11InsuranceClaims from '../lib/task-1_1-insurance-claims-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/task-1_1-insurance-claims-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Task11InsuranceClaims.Task11InsuranceClaimsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
